87e97152195c323a8ada7f9331bdf88f2763b4db
Emir Gökdemir <44062527+gokdeemir@users.noreply.github.com>
2020-05-11 05:55:37 +0300

    Sales Agreement is created in Turkish

--- epay-cloud/src/main/java/com/lens/epay/model/resource/organization/FirmResource.java
+++ epay-cloud/src/main/java/com/lens/epay/model/resource/organization/FirmResource.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+
 /**
  * Created by Emir Gökdemir
  * on 1 Mar 2020
@@ -18,4 +20,11 @@ public class FirmResource extends AbstractResource {
     private String city;
 
     private String taxId;
+
+    private String Address;
+
+    private String phoneNo;
+
+    @Email
+    private String email;
 }
