cb90a54d19f1c0a4f020d633e616bd5e6abda97b
Emir Gökdemir <44062527+gokdeemir@users.noreply.github.com>
2020-11-16 12:04:58 +0300

    Exceptions are edited.

--- epay-cloud/src/main/java/com/lens/epay/exception/BadRequestException.java
+++ epay-cloud/src/main/java/com/lens/epay/exception/BadRequestException.java
@@ -3,14 +3,14 @@ package com.lens.epay.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Created by Emir Gökdemir
  * on 20 Şub 2020
  */
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class BadRequestException extends HttpClientErrorException {
+public class BadRequestException extends ResponseStatusException {
 
     public BadRequestException(@Nullable String statusText) {
         super(HttpStatus.BAD_REQUEST, statusText);
--- epay-cloud/src/main/java/com/lens/epay/exception/NotFoundException.java
+++ epay-cloud/src/main/java/com/lens/epay/exception/NotFoundException.java
@@ -3,14 +3,14 @@ package com.lens.epay.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Created by Emir Gökdemir
  * on 9 May 2020
  */
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class NotFoundException extends HttpClientErrorException {
+public class NotFoundException extends ResponseStatusException {
 
     public NotFoundException(@Nullable String statusText) {
         super(HttpStatus.NOT_FOUND, statusText);
